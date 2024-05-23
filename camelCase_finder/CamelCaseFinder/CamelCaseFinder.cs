2374b3cf8022e9b809a5c5f06a309d8e4e9df145
Isy Reyes <132601984+IsyReyes@users.noreply.github.com>
2024-05-22 23:23:54 -0600

    implemented regex for parsing camelCase

--- camelCase_finder/CamelCaseFinder/CamelCaseFinder.cs
+++ camelCase_finder/CamelCaseFinder/CamelCaseFinder.cs
@@ -13,8 +13,28 @@ public class CamelCaseFinder{
         }else{
             Console.WriteLine($"{sourceDirectory} does exist");
         }
+
+        var regexCamelCase = new Regex(@"\b[a-z]+([A-Z][a-z]*)+\b");
+
+        foreach(var file in Directory.GetFiles(sourceDirectory, "*.cs", SearchOption.AllDirectories)){
+            Console.WriteLine($"Scanning file {file}");
+
+            var content = File.ReadAllText(file);
+            var matches = regexCamelCase.Matches(content);
+
+            if (matches.Count > 0){
+                Console.WriteLine($"Found {matches.Count} camelCase matches in {file}:");
+
+                foreach (Match match in matches){
+                    Console.WriteLine($"   -  {match.Value} at position {match.Index}");
+                }
+            }else{
+                Console.WriteLine("No camelCase matches found");
+            }
+        }
     }
 
-    
+
+
 
 }
